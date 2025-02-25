af5fd13ac0da290ba2525be37ca3f2e229e45266
李天华 <litianhua@moresec.cn>
2025-02-25 10:42:34 +0800

    picker

---
+++ lua/mru/picker.lua
@@ -0,0 +1,46 @@
+local M = {}
+
+M.mru = function(opts, _)
+  local current_file = vim.fs.normalize(vim.api.nvim_buf_get_name(0), { _fast = true })
+  local cwd = vim.uv.cwd() .. "/"
+  local limit = opts.limit or 10
+
+  local files = require("mru").load()
+
+  local filter = function(file)
+    if not vim.startswith(file, cwd) then
+      return false
+    end
+
+    if file == current_file then
+      return false
+    end
+
+    local file_stat = vim.uv.fs_stat(file)
+    if not file_stat or file_stat.type ~= "file" then
+      return false
+    end
+
+    return true
+  end
+
+  local results = {}
+
+  for _, file in ipairs(files) do
+    if filter(file) then
+      table.insert(results, file)
+
+      if #results >= limit then
+        break
+      end
+    end
+  end
+
+  return function(cb)
+    for _, file in ipairs(results) do
+      cb({ file = file, text = file })
+    end
+  end
+end
+
+return M
